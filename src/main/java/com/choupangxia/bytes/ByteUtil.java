4748225a5e850fc9abb9466f48f716ba3d426a73
secbro <xinyoulingxi2008@126.com>
2020-12-22 17:50:43 +0800

    修改方法名

--- src/main/java/com/choupangxia/bytes/ByteUtil.java
+++ src/main/java/com/choupangxia/bytes/ByteUtil.java
@@ -50,7 +50,7 @@ public class ByteUtil {
      * @param value 要转换的int值
      * @return byte数组
      */
-    public static byte[] intToBytes(int value) {
+    public static byte[] intToBytesLowAhead(int value) {
         byte[] src = new byte[4];
         src[3] = (byte) ((value >> 24) & 0xFF);
         src[2] = (byte) ((value >> 16) & 0xFF);
@@ -63,7 +63,7 @@ public class ByteUtil {
      * 将int数值转换为占四个字节的byte数组，本方法适用于(高位在前，低位在后)的顺序。
      * 和bytesToInt2（）配套使用
      */
-    public static byte[] intToBytes2(int value) {
+    public static byte[] intToBytesHighAhead(int value) {
         byte[] src = new byte[4];
         src[0] = (byte) ((value >> 24) & 0xFF);
         src[1] = (byte) ((value >> 16) & 0xFF);
