b7086afb65c4cbecfbd93c69f2d973008b7ec2f2
secbro <xinyoulingxi2008@126.com>
2020-12-22 17:41:04 +0800

    新增ByteUtil工具类

---
+++ src/main/java/com/choupangxia/bytes/ByteUtil.java
@@ -0,0 +1,98 @@
+package com.choupangxia.bytes;
+
+/**
+ * Byte相关公共处理工具
+ *
+ * @author sec
+ * @version 1.0
+ * @date 2020/12/22
+ **/
+public class ByteUtil {
+
+    /**
+     * 合并两个字节数组到一个字节数组
+     *
+     * @param data1 字节数组1
+     * @param data2 字节数组2
+     * @return byte[] 合并后的字节数字
+     */
+    public static byte[] mergeBytes(byte[] data1, byte[] data2) {
+        byte[] result = new byte[data1.length + data2.length];
+        System.arraycopy(data1, 0, result, 0, data1.length);
+        System.arraycopy(data2, 0, result, data1.length, data2.length);
+        return result;
+    }
+
+    /**
+     * 合并多个字节数组到一个字节数组
+     *
+     * @param values 动态字节数字参数
+     * @return byte[] 合并后的字节数字
+     */
+    private static byte[] mergeBytes(byte[]... values) {
+        int lengthByte = 0;
+        for (byte[] value : values) {
+            lengthByte += value.length;
+        }
+        byte[] allBytes = new byte[lengthByte];
+        int countLength = 0;
+        for (byte[] b : values) {
+            System.arraycopy(b, 0, allBytes, countLength, b.length);
+            countLength += b.length;
+        }
+        return allBytes;
+    }
+
+    /**
+     * 将int数值转换为占四个字节的byte数组，本方法适用于(低位在前，高位在后)的顺序。
+     * 和bytesToInt()配套使用
+     *
+     * @param value 要转换的int值
+     * @return byte数组
+     */
+    public static byte[] intToBytes(int value) {
+        byte[] src = new byte[4];
+        src[3] = (byte) ((value >> 24) & 0xFF);
+        src[2] = (byte) ((value >> 16) & 0xFF);
+        src[1] = (byte) ((value >> 8) & 0xFF);
+        src[0] = (byte) (value & 0xFF);
+        return src;
+    }
+
+    /**
+     * 将int数值转换为占四个字节的byte数组，本方法适用于(高位在前，低位在后)的顺序。
+     * 和bytesToInt2（）配套使用
+     */
+    public static byte[] intToBytes2(int value) {
+        byte[] src = new byte[4];
+        src[0] = (byte) ((value >> 24) & 0xFF);
+        src[1] = (byte) ((value >> 16) & 0xFF);
+        src[2] = (byte) ((value >> 8) & 0xFF);
+        src[3] = (byte) (value & 0xFF);
+        return src;
+    }
+
+    /**
+     * byte数组中取int数值，本方法适用于(低位在前，高位在后)的顺序，和和intToBytes（）配套使用
+     *
+     * @param src    byte数组
+     * @param offset 从数组的第offset位开始
+     * @return int数值
+     */
+    public static int bytesToIntLowAhead(byte[] src, int offset) {
+        return (src[offset] & 0xFF)
+                | ((src[offset + 1] & 0xFF) << 8)
+                | ((src[offset + 2] & 0xFF) << 16)
+                | ((src[offset + 3] & 0xFF) << 24);
+    }
+
+    /**
+     * byte数组中取int数值，本方法适用于(低位在后，高位在前)的顺序。和intToBytes2（）配套使用
+     */
+    public static int bytesToIntHighAhead(byte[] src, int offset) {
+        return ((src[offset] & 0xFF) << 24)
+                | ((src[offset + 1] & 0xFF) << 16)
+                | ((src[offset + 2] & 0xFF) << 8)
+                | (src[offset + 3] & 0xFF);
+    }
+}
